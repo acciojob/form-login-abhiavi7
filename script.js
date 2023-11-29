858516552cd605c569e9d81b999e78b8b8a3d0e2
abhiavi7 <118717274+abhiavi7@users.noreply.github.com>
2023-11-29 11:50:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
-    //Write your code here
+var x=document.getElementById("form1");
+let name = ""
+for (var i=0;i<x.length;i++) {
+if (x.elements[i].value!='Submit')
+name += x.elements[i].value + " "
+}
 
+alert(name.substring(0,name.length-1));
 }
